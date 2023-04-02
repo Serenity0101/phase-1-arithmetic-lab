1cb12f697755bca85a2a9f49281158c229d75e0a
Serenity Parker <sarenity.parker01@gmail.com>
2023-04-02 18:32:11 -0500

    help

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const multiply = 62
+    const num1 = 2
+    const num2 = 31
+num1*num2
 
-// Write your code here
+const mod = 4
+    const num3 = 6
+    const num4 = 2
+num3 - num4
+
+const max = 20 
+
+function random() {
+    return Math.ramdom()
+} 
